c9f8742a0a0fc20d999fedbce9271ccaa5688eee
romchellis <oborinroma@yandex.ru>
2022-09-20 11:35:41 +0300

    Init commit

---
+++ src/test/java/transferrer/BankTransferrerTest.java
@@ -0,0 +1,19 @@
+package transferrer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BankTransferrerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+    @Test
+    void nameTest() {
+        assertEquals(1,1);
+    }
+}
